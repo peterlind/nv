24048d2bf6b31b85aca7603643115e3e4798012c
Peter Lind <peter@lind.be>
2023-04-19 22:31:49 +0200

    Change app init to use Sinatra::Application

--- lib/app.rb
+++ lib/app.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require "rubygems"
-require "bundler"
-Bundler.require(:default)
+require "sinatra/base"
 
 # app entrypoint
-class NorthVolt < Sinatra::Base
+class NorthVolt < Sinatra::Application
   get "/" do
     content_type :json
     { north: "volt" }.to_json
   end
+
+  run! if app_file == $0
 end
